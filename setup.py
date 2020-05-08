5836d82e6472280f657d2eeaf9a9f767a7ed6e25
JoeZuhusky <jzuhusky@gmail.com>
2020-05-07 21:18:28 -0400

    Client: rename package to zbaseballdata

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
-package_name = "zbaseball"
+package_name = "zbaseballdata"
 
 setup(
     name=package_name,
     version="0.0.1",
     description="A python client for the zBaseballData API",
-    keywords="client python api zbaseball baseball data zbaseballdata",
+    keywords="client retrosheet python api zbaseballdata baseball data zbaseballdata zbaseball",
     maintainer_email="joey@zbaseballdata.com",
     packages=find_packages(),
     install_requires=["requests>=2.23.0"],
